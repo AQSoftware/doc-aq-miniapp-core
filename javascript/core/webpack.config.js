5a2a292c87f2c79db1cac202ef8c3f3b2dc55c5a
Ryan Brozo <ryanbrozo@ryanbrozo.com>
2018-05-01 18:41:47 +0200

    Added minification through webpack

---
+++ javascript/core/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'aq-miniapp-core-0.0.14.min.js',
+    library: 'AQCore',
+    libraryTarget: 'window'
+  },
+  externals: [
+    'base64-js',
+    'hawk',
+    'text-encoding-utf-8',
+    'uuid',
+    'whatwg-fetch'
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: '/node_modules/', loader: "babel-loader" }
+    ]
+  },
+  mode: 'production'
+}
